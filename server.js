d73b63ab919fb8a108800574b1661f78d5129a85
DavidSiegmann <90595405+DavidSiegmann@users.noreply.github.com>
2021-10-22 09:26:30 -0700

    added start in package.json

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const notes = require("./routes/notes")
 app.use("/api/notes", notes)
 
 app.get("/", (req, res) => 
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+    res.sendFile(path.join(__dirname, "public/index.html"))
 );
 
 app.get("/notes", (req, res) => 
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
 app.listen(PORT, () =>
